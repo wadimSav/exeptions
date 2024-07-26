4adfb28710fd47c3fbb5bc0772755d38b91c9b2f
Wadim Savickii <artemon271@ya.ru>
2024-07-26 15:25:18 +0500

    feat: app

---
+++ DateException.java
@@ -0,0 +1,11 @@
+public class DateException extends Throwable {
+    public DateException() {
+    }
+
+    public void dataException(String i) {
+        System.out.println("Exception: DateNotCorrectFormat");
+        System.out.printf("It`s not correct date format: %s", i);
+        System.out.println();
+    }
+
+}
